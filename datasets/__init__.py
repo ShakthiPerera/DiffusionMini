ad689180975d0b84cff2cae5bd9f8f3bacd3a47e
ShakthiPerera <shakthiperera231@gmail.com>
2025-04-07 13:57:21 +0530

    add datasets to init file

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -5,3 +5,12 @@ from .banana_with_two_circles import BananaWithTwoCirclesDataset
 from .moons import MoonDataset
 from .s_curve import SCurveDataset
 from .swiss_roll import SwissRollDataset
+from .banana import BananaDataset
+from .central_banana import CentralBanana
+from .moon_with_scatterings import MoonWithScatterings
+from .moon_with_two_circles_bounded import MoonWithTwoCiclesBounded
+from .moon_with_two_circles_unbounded import MoonWithTwoCircles
+from .multimodal_gaussians import MultimodalGasussians
+from .star_fish_decay import StarFishDecay
+from .star_fish_uniform import StarFishUniform
+from .two_rings_bounded import TwoRingsBounded
